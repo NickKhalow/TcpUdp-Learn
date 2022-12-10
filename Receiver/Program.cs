af50832ed29b28457a45bea50dfebfec940952e3
Nick Khalow <71646502+NickKhalow@users.noreply.github.com>
2022-12-10 14:09:40 +0400

    Initial commit

---
+++ Receiver/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Receiver;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        switch (args.First())
+        {
+            case "udp":
+                Udp();
+                break;
+            case "tcp":
+                Tcp();
+                break;
+            default:
+                throw new ArgumentException();
+        }
+    }
+
+
+    private static void Udp()
+    {
+        var localIP = new IPAddress(
+            new byte[] {127, 0, 0, 1}
+        );
+        var client = new UdpClient(
+            new IPEndPoint(
+                localIP,
+                25555
+            )
+        );
+        var endPoint = new IPEndPoint(
+            localIP,
+            25550
+        );
+        Console.WriteLine(
+            Encoding.ASCII.GetString(
+                client.Receive(ref endPoint)
+            )
+        );
+    }
+
+
+    private static void Tcp()
+    {
+        var listener = new TcpListener(
+            new IPAddress(new byte[] {127, 0, 0, 1}),
+            25550
+        );
+        Console.WriteLine("Create listener");
+        listener.Start();
+        Console.WriteLine("Listener started");
+        var connected = listener.AcceptTcpClient();
+        Console.WriteLine("Connection received");
+        connected.GetStream().WriteByte(166);
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Sender;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        switch (args.First())
+        {
+            case "udp":
+                Udp();
+                break;
+            case "tcp":
+                Tcp();
+                break;
+            default:
+                throw new ArgumentException();
+        }
+    }
+
+
+    private static void Udp()
+    {
+        var client = new UdpClient(
+            new IPEndPoint(
+                new IPAddress(
+                    new byte[] {127, 0, 0, 1}
+                ),
+                25550
+            )
+        );
+        client.Connect("localhost", 25555);
+        client.Send(
+            new ReadOnlySpan<byte>(
+                Encoding.ASCII.GetBytes(
+                    "Hello network!"
+                )
+            )
+        );
+    }
+
+
+    private static void Tcp()
+    {
+        var client = new TcpClient(
+            "localhost",
+            25550
+        );
+        Console.WriteLine(client.GetStream().ReadByte());
+    }
+}
